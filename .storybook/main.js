691c9a072420dcffec3c97ee87bcbe314afc1795
Nick Taylor <nick@iamdeveloper.com>
2021-10-22 23:22:46 -0400

    Added webpack alias/TypeScript path for @components.

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,4 +4,15 @@ module.exports = {
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@components': path.resolve(__dirname, '../components'),
+      },
+    };
+
+    return config;
+  },
 };
