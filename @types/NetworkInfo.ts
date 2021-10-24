b048a3abd93145a5da5eb9a45e25ddbe3d40c232
Nick Taylor <nick@iamdeveloper.com>
2021-10-23 22:54:25 -0400

    Improved the NetworkInfo type.

--- @types/NetworkInfo.ts
+++ @types/NetworkInfo.ts
@@ -6,14 +6,25 @@ import {
   RopstenNetworkId,
 } from '../utilities/NetworkIds';
 
-export type EthereumNetwork =
-  | typeof MainnetNetworkId
-  | typeof KovanNetworkId
-  | typeof RopstenNetworkId
-  | typeof RinkebyNetworkId
-  | typeof GoerliNetworkId;
-
-export interface NetworkInfo {
-  chainId: string; // Ethereum chain id in hexadecimal
-  networkVersion: EthereumNetwork;
-}
+export type NetworkInfo = Readonly<
+  | {
+      chainId: '0x1';
+      networkVersion: typeof MainnetNetworkId;
+    }
+  | {
+      chainId: '0x2a';
+      networkVersion: typeof KovanNetworkId;
+    }
+  | {
+      chainId: '0x3';
+      networkVersion: typeof RopstenNetworkId;
+    }
+  | {
+      chainId: '0x4';
+      networkVersion: typeof RinkebyNetworkId;
+    }
+  | {
+      chainId: '0x5';
+      networkVersion: typeof GoerliNetworkId;
+    }
+>;
